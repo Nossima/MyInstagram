08fbe2507f988b6875682291778ee27ec6321fc2
DarkCalbute <71269492+DarkCalbute@users.noreply.github.com>
2022-02-22 17:26:22 +0100

    front ui without error handling

--- Frontend/App.tsx
+++ Frontend/App.tsx
@@ -1,22 +1,11 @@
-import { StyleSheet } from 'react-native';
-import { AppStack } from './navigation/appNavigator';
 import React from 'react';
-import { Navigation } from './navigation/navigation';
+import { AppStack } from './navigation/appNavigator';
 import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
     return (
-        <NavigationContainer style={ styles.container }>
-            <AppStack />
+        <NavigationContainer>
+            <AppStack/>
         </NavigationContainer>
     );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- Frontend/navigation/appNavigator.tsx
+++ Frontend/navigation/appNavigator.tsx
@@ -3,15 +3,17 @@ import { Navigation } from './navigation';
 import { EditProfileScreen } from '../screens/editProfileScreen';
 import { HomeScreen } from '../screens/homeScreen';
 import { ProfileScreen } from '../screens/profileScreen';
+import { Register } from '../screens/authentification/register';
 
 const Stack = createNativeStackNavigator();
 
 export const AppStack = () => {
     return (
-        <Stack.Navigator initialRouteName={Navigation.Profile} screenOptions={{ headerShown: false}}>
+        <Stack.Navigator initialRouteName={Navigation.Register} screenOptions={{ headerShown: false}}>
             <Stack.Screen name={Navigation.EditProfile} component={EditProfileScreen} />
             <Stack.Screen name={Navigation.Home} component={HomeScreen} />
             <Stack.Screen name={Navigation.Profile} component={ProfileScreen} />
+            <Stack.Screen name={Navigation.Register} component={Register}/>
         </Stack.Navigator>
     );
 }
