f40e6108994eaf366d18e05104ae4ad81ba2f8f9
DarkCalbute <71269492+DarkCalbute@users.noreply.github.com>
2022-02-28 21:45:18 +0100

    merge fix

--- Frontend/App.tsx
+++ Frontend/App.tsx
@@ -3,13 +3,10 @@ import { AppStack } from './navigation/appNavigator';
 import { NavigationContainer } from '@react-navigation/native';
 import axios from 'axios';
 
-<<<<<<< HEAD
 export let API = axios.create({
-    baseURL: "http://192.168.1.18:3000"
+    baseURL: "http://localhost:3000"
 });
 
-=======
->>>>>>> 5fb8671d14ad796f07910ea0d1eb7c67f15e66cb
 export default function App() {
     return <NavigationContainer>
         <AppStack/>
--- Frontend/screens/authentification/register.tsx
+++ Frontend/screens/authentification/register.tsx
@@ -2,12 +2,9 @@ import React from "react";
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 import { View, Text, StyleSheet, TouchableOpacity, Image, Switch } from "react-native";
 import { TextInputField } from "../../component/TextInputField";
-<<<<<<< HEAD
 import * as SecureStore from 'expo-secure-store';
 
 import { authentificationService } from "../../service/authentification/";
-=======
->>>>>>> 5fb8671d14ad796f07910ea0d1eb7c67f15e66cb
 
 export const Register: React.VFC<any> = ({ navigation }) => {
     let [username, onChangeUsername] = React.useState('');
@@ -58,7 +55,6 @@ export const Register: React.VFC<any> = ({ navigation }) => {
         if (isUsernameError || isEmailError || isPasswordError || isCheckPasswordError) {
             return false;
         }
-<<<<<<< HEAD
         return register();
     };
 
@@ -114,11 +110,6 @@ export const Register: React.VFC<any> = ({ navigation }) => {
         });
     }
 
-=======
-        return true;
-    };
-
->>>>>>> 5fb8671d14ad796f07910ea0d1eb7c67f15e66cb
     return <View style={styles.background}>
         <View style={{alignItems: 'center'}}>
             <Image style={styles.logo} source={require('../../assets/Instagram_icon.png')}/>
