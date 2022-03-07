ee0dcc72cf038a58245aac9d0958553dd079ad0f
DarkCalbute <71269492+DarkCalbute@users.noreply.github.com>
2022-03-07 04:34:26 +0100

    merge fix v4

--- Frontend/App.tsx
+++ Frontend/App.tsx
@@ -4,17 +4,11 @@ import { NavigationContainer } from '@react-navigation/native';
 import axios from 'axios';
 
 export let API = axios.create({
-<<<<<<< HEAD
-    baseURL: "http://192.168.1.136:3000"
-});
-
-=======
     baseURL: "http://10.10.10.103:3000"
 });
 
 global.username = '';
 
->>>>>>> profile
 export default function App() {
     return <NavigationContainer>
         <AppStack/>
